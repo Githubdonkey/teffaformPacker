4e4dca712ef9bff56262a0390395761dd83e8496
Githubdonkey <30330841+Githubdonkey@users.noreply.github.com>
2019-09-28 15:08:21 -0400

    Add files via upload

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,1 +1,19 @@
+resource "aws_s3_bucket" "mainpublic" {
+  count = var.enable_publicAcess ? 1 : 0
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
+  tags = {
+    Name        = "My bucket public"
+    Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket" "mainprivate" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket private"
+    Environment = "Dev"
+  }
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,4 @@
+variable "enable_publicAcess" {
+  description = "If set to true, enable Public Access"
+  type        = bool
+}
