412f45e0aabebffbb55fa142def43315751d1dd0
Githubdonkey <30330841+Githubdonkey@users.noreply.github.com>
2019-09-28 17:31:15 -0400

    Add files via upload

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "linuxU1804" {
   tags = {
     Name = "Ubuntu_18.04"
   }
-
+}
 resource "aws_instance" "win2016" {
   count = var.createInstanceWin ? 1 : 0
   ami           = "ami-07d0cf3af28718ef8"
